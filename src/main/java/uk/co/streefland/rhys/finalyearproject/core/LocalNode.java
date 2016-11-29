6533d91e7b16a4aec73c3c790d862b3e8ac80e81
Rhys Streefland <rhysaled.streefland@ntlworld.com>
2016-11-29 14:57:03 +0000

    Initial styling effort
    Needs a fair bit more work

--- src/main/java/uk/co/streefland/rhys/finalyearproject/core/LocalNode.java
+++ src/main/java/uk/co/streefland/rhys/finalyearproject/core/LocalNode.java
@@ -18,7 +18,7 @@ import java.util.Timer;
  */
 public class LocalNode implements Runnable {
 
-    private static final String BUILD_NUMBER = "544";
+    private static final String BUILD_NUMBER = "670";
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     /* DHT objects */
